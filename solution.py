69b1186ac7b2f6d9f5567d6093af6d723b7a53ed
bk2557 <91108114+bk2557@users.noreply.github.com>
2022-02-03 22:02:11 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -22,9 +22,7 @@ def welcome_assignment_answers(question):
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
         answer = 5
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numberic number":
-        answer = 3
-    else:
-        answer = "No question"
+        answer = 4
     return(answer)
 # Complete all the questions.
 
